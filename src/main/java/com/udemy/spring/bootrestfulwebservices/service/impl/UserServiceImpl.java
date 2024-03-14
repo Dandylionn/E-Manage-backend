021bf141516675bfab57658adf1300690e0d9ca9
unknown <zheng.hui94@gmail.com>
2024-03-14 19:29:12 +0800

    added mapstruct for mapping

--- src/main/java/com/udemy/spring/bootrestfulwebservices/service/impl/UserServiceImpl.java
+++ src/main/java/com/udemy/spring/bootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -2,7 +2,7 @@ package com.udemy.spring.bootrestfulwebservices.service.impl;
 
 import com.udemy.spring.bootrestfulwebservices.dto.UserDto;
 import com.udemy.spring.bootrestfulwebservices.entity.User;
-import com.udemy.spring.bootrestfulwebservices.mapper.UserMapper;
+import com.udemy.spring.bootrestfulwebservices.mapper.AutoUserMapper;
 import com.udemy.spring.bootrestfulwebservices.repository.UserRepository;
 import com.udemy.spring.bootrestfulwebservices.service.UserService;
 import lombok.AllArgsConstructor;
@@ -25,13 +25,21 @@ public class UserServiceImpl implements UserService {
 
         //Convert UserDto into User JPA Entity
         //User user = UserMapper.mapToUser(userDto);
-        User user = modelMapper.map(userDto, User.class);
+//        User user = modelMapper.map(userDto, User.class); //ORIGINAL MAPPING METHOD
+
+        //Mapstruct method
+        User user = AutoUserMapper.MAPPER.mapToUser(userDto);
 
         User savedUser = userRepository.save(user);
 
         //Convert User JPA entity to UserDto
 //        UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
-        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
+//        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class); //ORI MAPPING METHOD
+
+        //Mapstruct method
+        UserDto savedUserDto = AutoUserMapper.MAPPER.mapToUserDto(savedUser);
+
+
         return savedUserDto;
     }
 
@@ -40,7 +48,12 @@ public class UserServiceImpl implements UserService {
         Optional<User> optionalUser = userRepository.findById(userId);
         User user = optionalUser.get();
 //        return UserMapper.mapToUserDto(user);
-        return modelMapper.map(user, UserDto.class);
+
+        //original mapping method
+//        return modelMapper.map(user, UserDto.class);
+
+        //Mapstruct method
+        return AutoUserMapper.MAPPER.mapToUserDto(optionalUser.get());
     }
 
     @Override
@@ -48,7 +61,13 @@ public class UserServiceImpl implements UserService {
         List<User> users = userRepository.findAll();
 //        return users.stream().map(UserMapper::mapToUserDto)
 //                .collect(Collectors.toList());
-        return users.stream().map((user) -> modelMapper.map(user, UserDto.class))
+
+        //ori mapping method
+//        return users.stream().map((user) -> modelMapper.map(user, UserDto.class))
+//                .collect(Collectors.toList());
+
+        //Mapstruct method
+        return users.stream().map((user) -> AutoUserMapper.MAPPER.mapToUserDto(user))
                 .collect(Collectors.toList());
     }
 
@@ -60,8 +79,14 @@ public class UserServiceImpl implements UserService {
         existingUser.setEmail(user.getEmail());
         User updatedUser = userRepository.save(existingUser);
 //        return UserMapper.mapToUserDto(updatedUser);
-        return modelMapper.map(updatedUser, UserDto.class);
+
+        //Ori mapping method
+//        return modelMapper.map(updatedUser, UserDto.class);
+
+        //Mapstruct method
+        return AutoUserMapper.MAPPER.mapToUserDto(updatedUser);
     }
+
 
     @Override
     public void deleteUser(Long userId) {
